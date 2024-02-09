dfde7e486b8567701bf8a78a59e04368a88147df
Drew <drewabril@gmail.com>
2024-02-09 15:16:19 -0500

    Rock Paper Scissors Done.

--- rockPaperScissors/rockPaperScissors.py
+++ rockPaperScissors/rockPaperScissors.py
@@ -1,20 +1,40 @@
 import random
 
 usrScore = 0
-
 cpuScore = 0
+options = ["rock", "paper", "scissors"]
 
 while True:
     usrInput = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if usrInput == 'q':
         break
-    if usrInput not in ["rock", "paper", "scissors"]
+    if usrInput not in options:
         print("Pick a valid resonse.")
         continue
-    r = random.randomint(0, 2)
+    r = random.randint(0, 2)
     #rock: 0, paper: 1, scissors: 2
+    cpuPick = options[r]
+    print("Computer Picked", cpuPick + ".")
 
+    if usrInput == 'rock' and cpuPick == "scissors":
+        print("You Won!")
+        usrScore += 1
+        continue
+    
+    elif usrInput == 'paper' and cpuPick == "rock":
+        print("You Won!")
+        usrScore += 1
+        continue
+    
+    elif usrInput == 'scissors' and cpuPick == "paper":
+        print("You Won!")
+        usrScore += 1
+        continue
+    else:
+        print("You Lost!")
+        cpuScore += 1
+
+print("You won", usrScore, "times.")
+print("You lost", cpuScore, "times.")
 print("Goodbye!")
-
-
 
