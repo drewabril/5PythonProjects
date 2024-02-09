7dcd8f01859201e18a81a9aa0dc1276473a4f66a
Drew <drewabril@gmail.com>
2024-02-09 15:34:11 -0500

    working on password manager

---
+++ passwordManager/passwordManager.py
@@ -0,0 +1,21 @@
+pwd = input("What is your password? ")
+
+def view():
+    pass
+
+def add():
+    pass
+
+
+
+while True:
+    mode = input("would you like to add, or view a password? (View/Add) press q to quit ").lower()
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    elif mode == "q":
+        quit()
+    else:
+        print("Not a valid mode")
+        continue
