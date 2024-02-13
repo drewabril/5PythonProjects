c093c8f28d0a6211ccdb37d992f5708e9852a8b3
Drew <drewabril@gmail.com>
2024-02-13 08:31:52 -0500

    progress on pw manager

--- passwordManager/passwordManager.py
+++ passwordManager/passwordManager.py
@@ -1,19 +1,36 @@
-pwd = input("What is your password? ")
+from cryptography.fernet import Fernet
+
+master_pwd = input("What is your password? ")
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+write_key()
+'''
 
 def view():
-    pass
-
+    with open ('pywords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip() #this strips off the \n carriage return from our add function
+            user, passw = data.split("|") #sets deliniator for key value pairs. same as user, passw = [drew, password]
+            print("User:", user, ", Password:", passw)
 def add():
-    pass
-
-
+    name = input('Account Name: ')
+    pwd = input('Password: ')
+    #use keyword 'with' to manipulate the file, open/close keywords are available but not as clean.
+    with open('pywords.txt', 'a') as f:
+# modes in with include Write, Read, Append (we picked a for append)
+        f.write(name + "|" + pwd + "\n")
 
 while True:
     mode = input("would you like to add, or view a password? (View/Add) press q to quit ").lower()
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     elif mode == "q":
         quit()
     else:
